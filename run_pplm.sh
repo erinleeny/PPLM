79f28ff5ace54f56c57d4983d47cf1c588840309
Erin Lee <erin.lee.ny@gmail.com>
2024-04-30 22:41:42 -0400

    bash file run

---
+++ run_pplm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+strings=("In summary" "This essay discusses" "Views on" "The connection" "Foundational to this is" "To review," "In brief," "An illustration of" "Furthermore," "The central theme" "To conclude," "The key aspect" "Prior to this" "Emphasised are" "To summarise" "The relationship" "More importantly," "It has been shown" "The issue focused on" "In this essay")
+files=("legal" "politics" "religion" "science" "technologies")
+
+for file in "${files[@]}"
+do
+    for str in "${strings[@]}"
+    do
+        python run_pplm.py -B $file --cond_text "$str" --length 50 --gamma 1.5 --num_iterations 3 --num_samples 1 --stepsize 0.03 --window_length 5 --kl_scale 0.01 --gm_scale 0.99 --colorama --sample --verbosity "quiet" >> "${file}_output.txt"
+    done
+done
